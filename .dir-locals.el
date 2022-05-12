ea1abab1c2b62d627eb7b0ae40d2830253cffea0
Christopher Rodriguez <yewscion@gmail.com>
2022-05-11 20:46:11 -0400

    Added .projectile and .dir-locals.el to repo.

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((projectile-project-compilation-cmd . "haunt build")
+         (projectile-project-name . "Yewscion Blog")
+         (projectile-project-run-cmd . "haunt serve")
+         (projectile-project-install-cmd . "rsync -avzh site/* cdr255.com:/srv/http/yewscion/")
+         )))
